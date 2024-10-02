b5efd0a83cceb430341ab9711d3b92cad46bf662
Daniel P <daniel@learnweaver.com>
2024-10-02 18:38:07 +1000

    LimeLight Wait Example

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenWaitAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/SpecimenWaitAuto.java
@@ -1,18 +1,23 @@
 package org.firstinspires.ftc.teamcode.opmodes.auto;
 import androidx.collection.ArraySet;
 
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.Vector2d;
+
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.commands.ActionCommand;
 import org.firstinspires.ftc.teamcode.subsystems.VisionSubsystem;
 import org.firstinspires.ftc.teamcode.utils.OTOSDrive;
+import org.firstinspires.ftc.teamcode.MecanumDrive.*;
+
 
 @Autonomous(name = "SpecimenWaitAuto", group = "Autonomous")
 public class SpecimenWaitAuto extends CommandOpMode {
@@ -26,21 +31,25 @@ public class SpecimenWaitAuto extends CommandOpMode {
 
     private Action pickupAction3;
 
+    private TrajectoryActionBuilder t0;
+    private TrajectoryActionBuilder t1;
+    private TrajectoryActionBuilder t2;
+
     private VisionSubsystem vision;
 
     @Override
     public void initialize() {
-
-        // instantiate your MecanumDrive at a particular pose.
+ // instantiate your MecanumDrive at a particular pose.
         OTOSDrive drive = new OTOSDrive(hardwareMap,
                 new Pose2d(25, -61, Math.toRadians(90)));
+
 
         vision = new VisionSubsystem(hardwareMap);
 
         vision.StartLimelight();
 
 
-        specimenAction = drive.actionBuilder(drive.pose)
+       t0 = drive.actionBuilder(drive.pose)
                 .strafeToConstantHeading(new Vector2d(47, -44))
                 .waitSeconds(0.25)
 
@@ -56,7 +65,7 @@ public class SpecimenWaitAuto extends CommandOpMode {
 
                 .strafeTo(new Vector2d(12,-37))
                 //.splineToLinearHeading(new Pose2d(10, -35, Math.toRadians(-90)), Math.toRadians(90))
-                .waitSeconds(0.25)
+                .waitSeconds(0.25);
 
                 /*.strafeTo(new Vector2d(35, -49))
                 .waitSeconds(0.25)
@@ -85,19 +94,25 @@ public class SpecimenWaitAuto extends CommandOpMode {
                 .strafeTo(new Vector2d(39, -53))
                 .waitSeconds(0.25)*/
 
-                .build();
+               // .build();
 
 
-        pickupAction1 = drive.actionBuilder(drive.pose)
+        t1 = t0.fresh()
                 .strafeTo(new Vector2d(35, -49))
-                .waitSeconds(0.25)
-                .build();
+                .waitSeconds(0.25);
 
-        dropOffAction1 = drive.actionBuilder(drive.pose)
+
+
+
+        t2 = t1.fresh()
                 .strafeTo(new Vector2d(12,-37))
-                .waitSeconds(0.25)
-                .build();
+                .waitSeconds(0.25);
 
+
+
+        specimenAction = t0.build();
+        pickupAction1 = t1.build();
+        dropOffAction1 = t2.build();
 
         CommandScheduler.getInstance().schedule(
                 new WaitUntilCommand(this::isStarted).andThen(
